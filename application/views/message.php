97cfe29f8d6be0f4f63cd0eac61e4d7ce6e5fce4
Jon Kuperman <jon@wojodesign.com>
2012-11-13 23:45:31 -0500

    fixing error in CSS link

--- application/views/message.php
+++ application/views/message.php
@@ -1,8 +1,8 @@
 <? $this->load->view('global/header_view'); ?>
         
 <?php
-	echo anchor('topics/index', 'Presentation Topics');
-	echo anchor('blog/index', 'Minutes From Past Meetups');
+	echo anchor('topics/index', 'Presentation Topics', 'class="btn btn-primary"');
+	echo anchor('blog/index', 'Minutes From Past Meetups', 'class="btn btn-primary"');
 ?>
 
 <? $this->load->view('global/footer_view'); ?>
