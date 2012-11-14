c98c7d953dc85454b2dbc29766963aa6d46485aa
Jon Kuperman <jon@wojodesign.com>
2012-11-13 23:55:42 -0500

    installing the REAL bootstrap

--- application/views/message.php
+++ application/views/message.php
@@ -1,7 +1,7 @@
 <? $this->load->view('global/header_view'); ?>
         
 <?php
-	echo anchor('topics/index', 'Presentation Topics', 'class="btn btn-primary"');
+	echo anchor('topics/show_all', 'Presentation Topics', 'class="btn btn-primary"');
 	echo anchor('blog/index', 'Minutes From Past Meetups', 'class="btn btn-primary"');
 ?>
 
