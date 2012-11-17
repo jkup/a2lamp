b1e28e99589c4e50723a0da12c327c39826492f6
Jon Kuperman <jon@wojodesign.com>
2012-11-16 19:23:05 -0500

    adding foundation setup to all views

--- application/views/blog_view.php
+++ application/views/blog_view.php
@@ -1,21 +1,20 @@
-<html>
-	<head>
-		<title>Codeigniter Test Day</title>
-	</head>
-	<body>
-	<?php
-		foreach($posts as $post)
-		{
-			echo "<h1>".$post->title."</h1>";
-			echo "<br />";
-			echo $post->content;
-			echo "<br />";
-			echo $post->author;
-			echo "<br />";
-			echo $post->timestamp;
-		}
-	?>
+<? $this->load->view('global/header_view'); ?>
+<div class="row">
+	<div class="six columns centered"> 
+		<?php
+			foreach($posts as $post)
+			{
+				echo "<h1>".$post->title."</h1>";
+				echo "<br />";
+				echo $post->content;
+				echo "<br />";
+				echo $post->author;
+				echo "<br />";
+				echo $post->timestamp;
+			}
+		?>
+	</div>
+</div>
 	<br />
 	<?php echo anchor("blog/create", "Create Post"); ?>
-	</body>
-</html>
+<? $this->load->view('global/footer_view'); ?>
--- application/views/topics/show_view.php
+++ application/views/topics/show_view.php
@@ -1,8 +1,11 @@
 <? $this->load->view('global/header_view'); ?>
-        
-<div class="topic">
-    <h2><?= $topic->title ?></h2>
-    <p><?= $topic->description ?></p>
+<div class="row">
+	<div class="six columns centered">       
+		<div class="topic">
+		    <h2><?= $topic->title ?></h2>
+		    <p><?= $topic->description ?></p>
+		</div>
+	</div>
 </div>
 
 <? $this->load->view('global/footer_view'); ?>
