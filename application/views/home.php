fdc17edf7b793a6054e869abd4ef4cc19b85dfd8
Kelly King <kelly.danielle.king@gmail.com>
2013-02-28 22:51:18 -0500

    added link to official meetup page on home page

--- application/views/home.php
+++ application/views/home.php
@@ -4,6 +4,7 @@
     <div class="twelve columns">  
         <h2>Welcome to the official website of the Ann Arbor PHP Meetup Group.</h2>
         <h3 class="subheader">Check out out blog and topics list and stay tuned for more updates!</h3>
+        <h4>Check out <a href="http://www.meetup.com/ann-arbor-php-mysql/">Our Meetup Site</a> for updates!</h4>
     </div>
 </div>
 
