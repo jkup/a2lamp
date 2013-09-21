e3a853bea773f5ef9994ca6eb26836a7cec69fce
Dan Trenz <dtrenz@gmail.com>
2013-09-21 17:48:31 -0400

    Added routes + controller methods for create and show

--- application/controllers/jobs.php
+++ application/controllers/jobs.php
@@ -8,4 +8,14 @@ class Jobs extends CI_Controller
         $this->load->view( 'jobs/all_view' );
     }
 
+    public function create()
+    {
+        $this->load->view( 'jobs/create_view' );
+    }
+
+    public function show( $job_id )
+    {
+        $this->load->view( 'jobs/show_view' );
+    }
+
 }
