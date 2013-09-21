823b7d22d170d726a815e7bba29ac39084fde369
Kelly King <kelly.danielle.king@gmail.com>
2013-09-21 18:11:31 -0400

    add dummy data to job listing view

--- application/views/jobs/all_view.php
+++ application/views/jobs/all_view.php
@@ -1,3 +1,48 @@
 <? $this->load->view('global/header_view'); ?>
 
+<div class="row">        
+	<table class="jobs">
+		<tr>
+			<th>Title</th>
+			<th>Company</th>
+			<th>Location</th>
+			<th>Date Posted</th>
+			<th>Apply</th>
+			<th>Description</th>
+		</tr>
+		<tr class="job">
+			<td>Job Title #1</td>
+			<td>Some Company</td>
+			<td>Ann Arbor, MI</td>
+			<td>September 20th, 2013</td>
+			<td><a href="#">Apply</a></td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, beatae corrupti sed quas sit rerum et architecto dolores impedit delectus iusto laboriosam ullam maiores deserunt.</td>
+		</tr>
+		<tr class="job">
+			<td>Job Title #2</td>
+			<td>Another Company</td>
+			<td>Farmington Hills, MI</td>
+			<td>September 20th, 2013</td>
+			<td><a href="#">Apply</a></td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, esse beatae deserunt quidem nihil nostrum dolorem voluptates fugiat quo ea?</td>
+		</tr>
+		<tr class="job">
+			<td>Job Title #3</td>
+			<td>Company #3</td>
+			<td>Plymouth, MI</td>
+			<td>September 20th, 2013</td>
+			<td><a href="#">Apply</a></td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, laboriosam, similique, aliquid eius praesentium recusandae assumenda quos natus esse minus nam ullam mollitia vitae vero vel error aliquam magni voluptatibus nulla beatae tempore reiciendis nobis qui dicta odit excepturi nesciunt doloribus quisquam hic voluptatem consequatur veritatis accusantium corporis iure doloremque!</td>
+		</tr>
+		<tr class="job">
+			<td>Job Title #4</td>
+			<td>Company #4</td>
+			<td>Detroit, MI</td>
+			<td>September 20th, 2013</td>
+			<td><a href="#">Apply</a></td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, tempora?</td>
+		</tr>
+	</table>
+</div>
+
 <? $this->load->view('global/footer_view'); ?>
