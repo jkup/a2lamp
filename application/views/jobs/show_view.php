f327a65d1eff365f646f37c1e394d8d98ccdae34
Kelly King <kelly.danielle.king@gmail.com>
2013-09-21 18:22:25 -0400

    add dummy data to single job posting view

--- application/views/jobs/show_view.php
+++ application/views/jobs/show_view.php
@@ -1,3 +1,18 @@
 <? $this->load->view('global/header_view'); ?>
 
+<div class="row">        
+	<h2>Job Title #1</h2>
+	<h3><a href="#">Apply</a></h3>
+</div>
+<div class="row">
+	<ul>
+		<li>Some Company</li>
+		<li>Ann Arbor, MI</li>
+		<li>September 20th, 2013</li>
+	</ul>
+</div>
+<div class="row">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, beatae corrupti sed quas sit rerum et architecto dolores impedit delectus iusto laboriosam ullam maiores deserunt.</p>
+<div>
+
 <? $this->load->view('global/footer_view'); ?>
