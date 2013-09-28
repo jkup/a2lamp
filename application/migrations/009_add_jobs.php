c7e0a309b9c6c73dad8ac0942aaed8e2d7dad912
Dan Trenz <dtrenz@gmail.com>
2013-09-28 15:47:25 -0400

    Added jobs table migration

---
+++ application/migrations/009_add_jobs.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_jobs extends CI_Migration {
+
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'title' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => '255',
+            ),
+            'location' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => '255',
+            ),
+            'company' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => '255',
+            ),
+            'url' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => '255',
+            ),
+            'description' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => 1000
+            ),
+            'created' => array(
+                'type'           => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+            ),
+            'user_id' => array(
+                'type'           => 'INT',
+                'constraint'     => 20,
+                'unsigned'       => TRUE
+            )
+        ));
+
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_key('created');
+
+        $this->dbforge->create_table('jobs');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('jobs');
+    }
+
+}
