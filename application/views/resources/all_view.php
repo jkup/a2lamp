942a08851dbcba61e7cbfe7970c682c0100dd3d4
Dan Trenz <dtrenz@gmail.com>
2013-11-23 13:00:10 -0500

    Added section for MySQL clients/tools + fixed some list markup issues.

--- application/views/resources/all_view.php
+++ application/views/resources/all_view.php
@@ -10,50 +10,68 @@
         <div class="large-12 columns">
             <p>If you're looking to catch up with the technologies we use in class, check out these links!</p>
             <ul>
-                <li><h2>Text Editors / IDEs</h2></li>
-                <ul>
-                    <li><a href="http://www.sublimetext.com/2">Sublime Text 2</a></li>
-                    <li><a href="http://www.jetbrains.com/phpstorm/">PhpStorm</a></li>
-                    <li><a href="https://netbeans.org/">Netbeans</a></li>
-                    <li><a href="http://www.eclipse.org/">Eclipse</a></li>
-                    <li><a href="http://notepad-plus-plus.org/">Notepad++</a></li>
-                    <li><a href="http://www.vim.org/">Vim</a></li>
-                </ul>
-                <li><h2>Version Control ( we use git )</h2></li>
-                <ul>
-                    <li><a href="http://git-scm.com/">Git</a>
-                        <ul>
-                            <li><a href="http://rogerdudler.github.io/git-guide/">Git - the simple guide</a></li>
-                            <li><a href="https://www.codeschool.com/courses/try-git">Free git video series</a></li>
-                            <li><a href="https://github.com/">Github</a></li>
-                            <li><a href="http://stackoverflow.com/questions/11816424/understanding-the-basics-of-git-and-github">Git vs. Github</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="http://subversion.apache.org/">Subversion</a></li>
-                    <li><a href="http://mercurial.selenic.com/">Mercurial</a></li>
-                </ul>
-                <li><h2>Development Environments</h2></li>
-                <ul>
-                    <li><a href="http://www.wampserver.com/en/">Windows</a></li>
-                    <li><a href="http://www.mamp.info/en/index.html">Mac</a></li>
-                    <li><a href="http://www.howtoforge.com/ubuntu_lamp_for_newbies">Ubuntu</a></li>
-                    <li><a href="http://docs.vagrantup.com/v2/getting-started/index.html">Vagrant</a></li>
-                </ul>
-                <li><h2>Introduction to PHP</h2></li>
-                <ul>
-                    <li><a href="http://www.php.net/">Official Website</a></li>
-                    <li><a href="http://www.lynda.com/">Premium Video Tutorials</a></li>
-                    <li><a href="http://www.codecademy.com/tracks/php">Interactive Tutorials</a></li>
-                    <li><a href="http://net.tutsplus.com/tutorials/php/the-best-way-to-learn-php/">The best way to learn PHP</a></li>
-                </ul>
-                <li><h2>Frameworks</h2></li>
-                <ul>
-                    <li><a href="http://ellislab.com/codeigniter">CodeIgniter</a></li>
-                    <li><a href="http://laravel.com/">Laravel</a></li>
-                    <li><a href="http://symfony.com/">Symfony</a></li>
-                    <li><a href="http://silex.sensiolabs.org/">Silex ( micro-framework )</a></li>
-                    <li><a href="http://framework.zend.com/">Zend Framework</a></li>
-                </ul>
+                <li>
+                    <h2>Text Editors / IDEs</h2>
+                    <ul>
+                        <li><a href="http://www.sublimetext.com/2">Sublime Text 2</a></li>
+                        <li><a href="http://www.jetbrains.com/phpstorm/">PhpStorm</a></li>
+                        <li><a href="https://netbeans.org/">Netbeans</a></li>
+                        <li><a href="http://www.eclipse.org/">Eclipse</a></li>
+                        <li><a href="http://notepad-plus-plus.org/">Notepad++</a></li>
+                        <li><a href="http://www.vim.org/">Vim</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Version Control ( we use git )</h2>
+                    <ul>
+                        <li><a href="http://git-scm.com/">Git</a>
+                            <ul>
+                                <li><a href="http://rogerdudler.github.io/git-guide/">Git - the simple guide</a></li>
+                                <li><a href="https://www.codeschool.com/courses/try-git">Free git video series</a></li>
+                                <li><a href="https://github.com/">Github</a></li>
+                                <li><a href="http://stackoverflow.com/questions/11816424/understanding-the-basics-of-git-and-github">Git vs. Github</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="http://subversion.apache.org/">Subversion</a></li>
+                        <li><a href="http://mercurial.selenic.com/">Mercurial</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Development Environments</h2>
+                    <ul>
+                        <li><a href="http://www.wampserver.com/en/">Windows</a></li>
+                        <li><a href="http://www.mamp.info/en/index.html">Mac</a></li>
+                        <li><a href="http://www.howtoforge.com/ubuntu_lamp_for_newbies">Ubuntu</a></li>
+                        <li><a href="http://docs.vagrantup.com/v2/getting-started/index.html">Vagrant</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Introduction to PHP</h2>
+                    <ul>
+                        <li><a href="http://www.php.net/">Official Website</a></li>
+                        <li><a href="http://www.lynda.com/">Premium Video Tutorials</a></li>
+                        <li><a href="http://www.codecademy.com/tracks/php">Interactive Tutorials</a></li>
+                        <li><a href="http://net.tutsplus.com/tutorials/php/the-best-way-to-learn-php/">The best way to learn PHP</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Frameworks</h2>
+                    <ul>
+                        <li><a href="http://ellislab.com/codeigniter">CodeIgniter</a></li>
+                        <li><a href="http://laravel.com/">Laravel</a></li>
+                        <li><a href="http://symfony.com/">Symfony</a></li>
+                        <li><a href="http://silex.sensiolabs.org/">Silex ( micro-framework )</a></li>
+                        <li><a href="http://framework.zend.com/">Zend Framework</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Working with MySQL</h2>
+                    <ul>
+                        <li><a href="http://www.phpmyadmin.net">PHPMyAdmin</a></li>
+                        <li><a href="http://www.sequelpro.com">Sequel Pro</a></li>
+                        <li><a href="http://www.mysql.com/products/workbench/">MySQL Workbench</a></li>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
